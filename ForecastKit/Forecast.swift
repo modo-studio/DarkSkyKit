443d8a288a4cb2b709f12a0cb3116d3617ee8854
Sergi Gracia <sergigram@gmail.com>
2016-07-26 15:37:38 +0200

    Update Forecast model

--- ForecastKit/Forecast.swift
+++ ForecastKit/Forecast.swift
@@ -2,16 +2,16 @@ import Foundation
 
 public struct Forecast {
     
-    let latitude: Double
-    let longitude: Double
-    let timezone: String
-    let offset: Int
-    let currently: ForecastDataPoint
-    let minutely: [ForecastDataPoint]
-    let hourly: [ForecastDataPoint]
-    let daily: [ForecastDataPoint]
-    let alerts: [ForecastAlert]?
-    let flags: ForecastFlags?
+    var latitude: Double = 0
+    var longitude: Double = 0
+    var timezone: String = ""
+    var offset: Int = 0
+    var currently: ForecastDataPoint?
+    var minutely: [ForecastDataPoint]?
+    var hourly: [ForecastDataPoint]?
+    var daily: [ForecastDataPoint]?
+    var alerts: [ForecastAlert]?
+    var flags: ForecastFlags?
     
     init(data: [String:AnyObject]) {
         if let lat = data["latitude"] as? Double {
@@ -25,6 +25,24 @@ public struct Forecast {
         }
         if let offset = data["offset"] as? Int {
             self.offset = offset
+        }
+        if let currently = data["currently"] as? [String: AnyObject] {
+            self.currently = ForecastDataPoint.map(currently)
+        }
+        if let minutely = data["minutely"] as? [[String: AnyObject]] {
+            self.minutely = minutely.map(ForecastDataPoint.map)
+        }
+        if let hourly = data["hourly"] as? [[String: AnyObject]] {
+            self.hourly = hourly.map(ForecastDataPoint.map)
+        }
+        if let daily = data["daily"] as? [[String: AnyObject]] {
+            self.daily = daily.map(ForecastDataPoint.map)
+        }
+        if let alerts = data["alerts"] as? [[String: AnyObject]] {
+            self.alerts = alerts.map(ForecastAlert.map)
+        }
+        if let flags = data["hoflagsurly"] as? [String: AnyObject] {
+            self.flags = ForecastFlags.map(flags)
         }
     }
 }
