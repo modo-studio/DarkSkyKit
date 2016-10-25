ce84e6c9193159fae394e232d4e2a03760e3256e
Sergi Gracia <sergigram@gmail.com>
2016-10-25 20:21:45 +0200

    Migrate models to Swift 3

--- DarkSkyKit/Models/ForecastAlert.swift
+++ DarkSkyKit/Models/ForecastAlert.swift
@@ -2,14 +2,14 @@ import Foundation
 
 public struct ForecastAlert {
     var title: String?
-    var expires: NSDate?
+    var expires: Date?
     var description: String?
     var uri: String?
 
-    static func map(data: [String: AnyObject]) -> ForecastAlert {
+    static func map(_ data: [String: AnyObject]) -> ForecastAlert {
         var a = ForecastAlert()
         a.title = data["title"] as? String ?? nil
-        a.expires = NSDate(time: data["expires"] as? Double)
+        a.expires = Date(time: data["expires"] as? Double)
         a.description = data["description"] as? String ?? nil
         a.uri = data["uri"] as? String ?? nil
         return a
--- DarkSkyKit/Models/ForecastDataPoint.swift
+++ DarkSkyKit/Models/ForecastDataPoint.swift
@@ -1,30 +1,30 @@
 import Foundation
 
 public struct ForecastDataPoint {
-    var time: NSDate?
+    var time: Date?
     var summary: String?
     var icon: String?
-    var sunriseTime: NSDate?
-    var sunsetTime: NSDate?
+    var sunriseTime: Date?
+    var sunsetTime: Date?
     var moonPhase: Double?
     var nearestStormDistance: Double?
     var nearestStormBearing: Double?
     var precipIntensity: Double?
     var precipIntensityMax: Double?
-    var precipIntensityMaxTime: NSDate?
+    var precipIntensityMaxTime: Date?
     var precipProbability: Double?
     var precipType: String?
     var precipAccumulation: Double?
     var temperature: Double?
     var temperatureMin: Double?
-    var temperatureMinTime: NSDate?
+    var temperatureMinTime: Date?
     var temperatureMax: Double?
-    var temperatureMaxTime: NSDate?
+    var temperatureMaxTime: Date?
     var apparentTemperature: Double?
     var apparentTemperatureMin: Double?
-    var apparentTemperatureMinTime: NSDate?
+    var apparentTemperatureMinTime: Date?
     var apparentTemperatureMax: Double?
-    var apparentTemperatureMaxTime: NSDate?
+    var apparentTemperatureMaxTime: Date?
     var dewPoint: Double?
     var windSpeed: Double?
     var windBearing: Double?
@@ -34,32 +34,32 @@ public struct ForecastDataPoint {
     var visibility: Double?
     var ozone: Double?
     
-    static func map(data: [String: AnyObject]) -> ForecastDataPoint {
+    static func map(_ data: [String: AnyObject]) -> ForecastDataPoint {
         var p = ForecastDataPoint()
-        p.time = NSDate(time: data["time"] as? Double)
+        p.time = Date(time: data["time"] as? Double)
         p.summary = data["summary"] as? String ?? ""
         p.icon = data["icon"] as? String ?? ""
-        p.sunriseTime = NSDate(time: data["sunriseTime"] as? Double)
-        p.sunsetTime = NSDate(time: data["sunsetTime"] as? Double)
+        p.sunriseTime = Date(time: data["sunriseTime"] as? Double)
+        p.sunsetTime = Date(time: data["sunsetTime"] as? Double)
         p.moonPhase = data["moonPhase"] as? Double ?? nil
         p.nearestStormDistance = data["nearestStormDistance"] as? Double ?? nil
         p.nearestStormBearing = data["nearestStormBearing"] as? Double ?? nil
         p.precipIntensity = data["precipIntensity"] as? Double ?? nil
         p.precipIntensityMax = data["precipIntensityMax"] as? Double ?? nil
-        p.precipIntensityMaxTime = NSDate(time: data["precipIntensityMaxTime"] as? Double)
+        p.precipIntensityMaxTime = Date(time: data["precipIntensityMaxTime"] as? Double)
         p.precipProbability = data["precipProbability"] as? Double ?? nil
         p.precipType = data["precipType"] as? String ?? nil
         p.precipAccumulation = data["precipAccumulation"] as? Double ?? nil
         p.temperature = data["temperature"] as? Double ?? nil
         p.temperatureMin = data["temperatureMin"] as? Double ?? nil
-        p.temperatureMinTime = NSDate(time: data["temperatureMinTime"] as? Double)
+        p.temperatureMinTime = Date(time: data["temperatureMinTime"] as? Double)
         p.temperatureMax = data["temperatureMax"] as? Double ?? nil
-        p.temperatureMaxTime = NSDate(time: data["temperatureMaxTime"] as? Double)
+        p.temperatureMaxTime = Date(time: data["temperatureMaxTime"] as? Double)
         p.apparentTemperature = data["apparentTemperature"] as? Double ?? nil
         p.apparentTemperatureMin = data["apparentTemperatureMin"] as? Double ?? nil
-        p.apparentTemperatureMinTime = NSDate(time: data["apparentTemperatureMinTime"] as? Double)
+        p.apparentTemperatureMinTime = Date(time: data["apparentTemperatureMinTime"] as? Double)
         p.apparentTemperatureMax = data["apparentTemperatureMax"] as? Double ?? nil
-        p.apparentTemperatureMaxTime = NSDate(time: data["apparentTemperatureMaxTime"] as? Double)
+        p.apparentTemperatureMaxTime = Date(time: data["apparentTemperatureMaxTime"] as? Double)
         p.dewPoint = data["dewPoint"] as? Double ?? nil
         p.windSpeed = data["windSpeed"] as? Double ?? nil
         p.windBearing = data["windBearing"] as? Double ?? nil
