908e76fa722f9d01e8da6ee9d017a7840f281912
Sergi Gracia <sergigram@gmail.com>
2016-10-25 20:22:14 +0200

    Migrate public interface to Swift 3

--- DarkSkyKit/Current.swift
+++ DarkSkyKit/Current.swift
@@ -2,15 +2,15 @@ import Foundation
 import Alamofire
 
 public extension DarkSkyKit {
-    public func current(latitude lat: Double, longitude long: Double, result: Result<Forecast, NSError> -> Void) {
-        Alamofire.request(Router.Current(configuration, lat, long)).responseJSON { response in
+    public func current(latitude lat: Double, longitude long: Double, result: @escaping (Result<Forecast>) -> Void) {
+        Alamofire.request(Router.current(configuration, lat, long)).responseJSON { response in
             switch response.result {
-            case .Success(let value):
+            case .success(let value):
                 if let json = value as? [String:AnyObject] {
-                    result(Result.Success(Forecast(data: json)))
+                    result(Result.success(Forecast(data: json)))
                 }
-            case .Failure(let error):
-                result(Result.Failure(error))
+            case .failure(let error):
+                result(Result.failure(error))
             }
         }
     }
