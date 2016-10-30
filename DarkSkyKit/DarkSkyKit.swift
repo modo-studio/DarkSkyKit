ffbccda72231292eb3d56c637a001ce323e68774
Sergi Gracia <sergigram@gmail.com>
2016-10-30 19:32:47 +0100

    Update Kit initializers to force parameter name usage

--- DarkSkyKit/DarkSkyKit.swift
+++ DarkSkyKit/DarkSkyKit.swift
@@ -2,12 +2,12 @@ import Foundation
 
 public struct DarkSkyKit {
     public let configuration: Configuration
-    
-    public init(_ apiToken: String) {
+
+    public init(apiToken: String) {
         self.configuration = Configuration(token: apiToken)
     }
 
-    public init(_ configuration: Configuration) {
+    public init(configuration: Configuration) {
         self.configuration = configuration
     }
 }
